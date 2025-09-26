ea3c0610ce34a6b28ae4bca37f331fbb0d03ca93
aawilliams85 <adair.williams85@gmail.com>
2025-09-26 13:46:32 -0400

    Start to decode attributes.  Tracking one byte offset that causes failures (in instances?)

--- sputility/aaobject.py
+++ sputility/aaobject.py
@@ -24,27 +24,43 @@ class AaObjAttrTypeEnum(IntEnum):
     BigStringType = 16
 
 @dataclass
-class AaObj:
+class AaObjBin:
     data: bytes
     offset: int
 
 @dataclass
 class AaObjHeader:
     base_gobjectid: int
-    is_template: bool
+    is_template: bool       # <Obj>._IsTemplate
     this_gobjectid: int
-    security_group: str
+    security_group: str     # <Obj>.SecurityGroup
     parent_gobjectid: int
-    tagname: str
-    contained_name: str
-    config_version: int
-    hierarchal_name: str
-    host_name: str
-    container_name: str
-    area_name: str
+    tagname: str            # <Obj>.Tagname
+    contained_name: str     # <Obj>.ContainedName
+    config_version: int     # <Obj>.ConfigVersion
+    hierarchal_name: str    # <Obj>.HierarchalName
+    host_name: str          # <Obj>.Host
+    container_name: str     # <Obj>.Container
+    area_name: str          # <Obj>.Area
     derived_from: str
-    based_on: str
+    based_on: str           # <Obj>._BasedOn
     galaxy_name: str
+
+@dataclass
+class AaObjAttr:
+    name: str
+    datatype: AaObjAttrTypeEnum
+    parent_gobjectid: int
+    parent_name: str
+    data: bool | int | float | str
+
+@dataclass
+class AaObjSection:
+    main_section_id: int
+    template_name: str
+    attr_section_id: int
+    attr_count: int
+    attr_section: list[AaObjAttr]
 
 @dataclass
 class AaObjAttrHeader:
@@ -66,35 +82,55 @@ class AaObjAttrHeader:
     magic: bytes
     data: bytes
 
-def _seek_pad(input: AaObj, length: int):
+def _seek_pad(input: AaObjBin, length: int):
     input.offset += length
 
-def _seek_fixed_int(input: AaObj, length: int = 4) -> int:
+def _seek_float(input: AaObjBin) -> float:
+    length = 4
+    value = struct.unpack('<f', input.data[input.offset:input.offset + length])
+    input.offset += length
+    return value
+
+def _seek_double(input: AaObjBin) -> float:
+    length = 8
+    value = struct.unpack('<d', input.data[input.offset:input.offset + length])
+    input.offset += length
+    return value
+
+def _seek_int(input: AaObjBin, length: int = 4) -> int:
     value = int.from_bytes(input.data[input.offset:input.offset + length], 'little')
     input.offset += length
     return value
 
-def _seek_fixed_string(input: AaObj, length: int = 64) -> str:
+def _seek_int_var_len(input: AaObjBin, length: int = 4) -> int:
+    int_Len = int.from_bytes(input.data[input.offset: input.offset + length], 'little')
+    input.offset += length
+    length = int_Len
+    value = int.from_bytes(input.data[input.offset: input.offset + length], 'little')
+    input.offset += length
+    return value
+
+def _seek_string(input: AaObjBin, length: int = 64) -> str:
     value = input.data[input.offset: input.offset + length].decode(encoding='utf-16-le').rstrip('\x00')
     input.offset += length
     return value
 
-def _seek_variable_string(input: AaObj) -> str:
-    length = 4
+def _seek_string_var_len(input: AaObjBin, length: int = 4, mult: int = 1) -> str:
+    # Some variable-length string fields start with 4 bytes to specify the length in bytes.
+    # Other use 2 bytes to specify the length in characters.  For the latter specify length=2, mult=2.
     str_len = int.from_bytes(input.data[input.offset: input.offset + length], 'little')
     input.offset += length
-    length = str_len
+    length = str_len * mult
     value = input.data[input.offset: input.offset + length].decode(encoding='utf-16-le').rstrip('\x00')
     input.offset += length
     return value
 
-def get_header(input: AaObj) -> AaObjHeader:
-    # Base Object ID (ex: UserDefined or other built-in object)
-    base_gobjectid = _seek_fixed_int(input=input)
+def _get_header(input: AaObjBin) -> AaObjHeader:
+    base_gobjectid = _seek_int(input=input)
 
     # If this is a template there will be four null bytes
     # Otherwise if those bytes are missing, it is an instance
-    check_is_template = _seek_fixed_int(input=input)
+    check_is_template = _seek_int(input=input)
     if check_is_template:
         is_template = False
         input.offset -= 4
@@ -102,32 +138,34 @@ def get_header(input: AaObj) -> AaObjHeader:
         is_template = True
 
     _seek_pad(input=input, length=4)
-    this_gobjectid = _seek_fixed_int(input=input)
+    this_gobjectid = _seek_int(input=input)
     _seek_pad(input=input, length=12)
-    security_group = _seek_fixed_string(input=input)
+    security_group = _seek_string(input=input)
     _seek_pad(input=input, length=12)
-    parent_gobject_id = _seek_fixed_int(input=input)
+    parent_gobject_id = _seek_int(input=input)
     _seek_pad(input=input, length=52)
-    tagname = _seek_fixed_string(input=input)
+    tagname = _seek_string(input=input)
     _seek_pad(input=input, length=596)
-    contained_name = _seek_fixed_string(input=input)
+    contained_name = _seek_string(input=input)
     _seek_pad(input=input, length=4)
     _seek_pad(input=input, length=32)
-    config_version = _seek_fixed_int(input=input)
+    config_version = _seek_int(input=input)
     _seek_pad(input=input, length=16)
-    hierarchal_name = _seek_fixed_string(input=input, length=130)
+    hierarchal_name = _seek_string(input=input, length=130)
     _seek_pad(input=input, length=530)
-    host_name = _seek_fixed_string(input=input)
+    host_name = _seek_string(input=input)
     _seek_pad(input=input, length=2)
-    container_name = _seek_fixed_string(input=input)
+    container_name = _seek_string(input=input)
     _seek_pad(input=input, length=596)
-    area_name = _seek_fixed_string(input=input)
+    area_name = _seek_string(input=input)
     _seek_pad(input=input, length=2)
-    derived_from = _seek_fixed_string(input=input)
+    derived_from = _seek_string(input=input)
     _seek_pad(input=input, length=596)
-    based_on = _seek_fixed_string(input=input)
+    based_on = _seek_string(input=input)
     _seek_pad(input=input, length=528)
-    galaxy_name = _seek_variable_string(input=input)
+    galaxy_name = _seek_string_var_len(input=input)
+    print(f'Offset: {input.offset}')
+    _seek_pad(input=input, length=1354)
 
     return AaObjHeader(
         base_gobjectid=base_gobjectid,
@@ -147,6 +185,71 @@ def get_header(input: AaObj) -> AaObjHeader:
         galaxy_name=galaxy_name
     )
 
+def _get_attr(input: AaObjBin) -> AaObjAttr:
+    _seek_pad(input=input, length=4)
+    name = _seek_string_var_len(input=input, length=2, mult=2)
+    datatype = _seek_int(input=input, length=1)
+    _seek_pad(input=input, length=16)
+    parent_gobjectid = _seek_int(input=input, length=4)
+    _seek_pad(input=input, length=8)
+    parent_name = _seek_string_var_len(input=input, length=2, mult=2)
+    _seek_pad(input=input, length=2)
+    _seek_pad(input=input, length=17)
+    data = None
+    match datatype:
+        case AaObjAttrTypeEnum.NoneType.value:
+            raise NotImplementedError()
+        case AaObjAttrTypeEnum.BooleanType.value:
+            data = bool(_seek_int(input=input, length=1))
+        case AaObjAttrTypeEnum.IntegerType.value:
+            data = _seek_int(input=input, length=4)
+        case AaObjAttrTypeEnum.FloatType.value:
+            data = _seek_float(input=input)
+        case AaObjAttrTypeEnum.DoubleType.value:
+            data = _seek_double(input=input)
+        case AaObjAttrTypeEnum.StringType.value:
+            data = _seek_string_var_len(input=input)
+        case AaObjAttrTypeEnum.TimeType.value:
+            data = _seek_int_var_len(input=input)
+        case AaObjAttrTypeEnum.ElapsedTimeType.value:
+            data = _seek_int(input=input, length=8)
+        case AaObjAttrTypeEnum.InternationalizedStringType.value:
+            data = _seek_string_var_len(input=input)
+        case AaObjAttrTypeEnum.BigStringType.value:
+            raise NotImplementedError()
+        case _:
+            raise NotImplementedError()
+
+    return AaObjAttr(
+        name=name,
+        datatype=AaObjAttrTypeEnum(datatype),
+        parent_gobjectid=parent_gobjectid,
+        parent_name=parent_name,
+        data=data
+    )
+
+def _get_attrs(input: AaObjBin) -> AaObjSection:
+    main_section_id = _seek_int(input=input, length=16)
+    template_name = _seek_string(input=input)
+    _seek_pad(input=input, length=596)
+    attr_section_id = _seek_int(input=input, length=16)
+    print(f'Count Offset: {input.offset}')
+    attr_count = _seek_int(input=input)
+    print(attr_count)
+    attrs = []
+    if attr_count > 0:
+        for i in range(attr_count):
+            attr = _get_attr(input=input)
+            attrs.append(attr)
+
+    return AaObjSection(
+        main_section_id=main_section_id,
+        template_name=template_name,
+        attr_section_id=attr_section_id,
+        attr_count=attr_count,
+        attr_section=attrs
+    )
+
 def explode_aaobject(
     input: str | bytes,
     output_path: str
@@ -157,6 +260,7 @@ def explode_aaobject(
     aaobject_bytes: bytes
 
     if isinstance(input, (str, os.PathLike)):
+        print(input)
         try:
             with open(input, 'rb') as file:
                 aaobject_bytes = file.read()
@@ -167,10 +271,14 @@ def explode_aaobject(
     else:
         raise TypeError('Input must be a file path (str/PathLike) or bytes.')
     
-    aaobj = AaObj(
+    aaobj = AaObjBin(
         data=aaobject_bytes,
         offset=0
     )
-    header = get_header(input=aaobj)
+    header = _get_header(input=aaobj)
     pprint.pprint(header)
+    print('')
+
+    attrs = _get_attrs(input=aaobj)
+    pprint.pprint(attrs)
     print('')
