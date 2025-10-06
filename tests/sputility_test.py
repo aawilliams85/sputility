0711afa134cc78eeb9fa4e4e35b17949053b65d2
aawilliams85 <81110595+aawilliams85@users.noreply.github.com>
2025-10-05 21:29:22 -0400

    Initial exploration of package (*.aapkg) and object (*.txt) files. (#1)
    * Extract *.aaPKG to folder
    
    * Deserialize Manifest.xml
    
    * Add type enum.  Slight class rename.
    
    * Fix enum syntax
    
    * Begin decoding header bytes
    
    * Added more header fields
    
    * Added more header fields, condensed extraction
    
    * Start to decode attributes.  Tracking one byte offset that causes failures (in instances?)
    
    * Testing offset fix for instance attributes
    
    * Fixed handling for string values and first attempt at internationalized string values.  Added conversion for datetime values.
    
    * Added attribute value locked field
    
    * Added attribute permission
    
    * Added attribute writeability
    
    * Added attribute array handling for bools and ints
    
    * Added attribute array handling for floats, doubles, strings, times, elapsed times.  Added value handling to format elapsed times into timedeltas.
    
    * Reorganize into separate submodules for package and object files
    
    * Reorganized imports for object subpackage to be consistent with package
    
    * Fixed attr lock handling to include inherited locks.
    
    * Reorganize value object extraction
    
    * Fix attribute lock to use enum.  Store value object instead of just the value itself.
    
    * Incomplete parsing for QualifiedEnum and Reference types, and some built in (?) attributes
    
    * Moved object primitives into separate routine.  More WIP for QualifiedEnums.
    
    * More WIP on Reference type
    
    * Add attribute id.  Attempt to fix built-in attribute deserialization for locked children.  Fix string alignment in reference type.
    
    * Fix naming for QualifiedEnum members
    
    * Partially added attr breakdown for Status, QualifiedStruct, DataType Array.  More WIP working through attribute sections of object.
    
    * Start to deserialize scripts
    
    * Added some return info to help see how much of the aaobj file is left
    
    * Addition work on isolating ScriptExtension and InputExtension sections
    
    * Check that _seek_bytes doesn't exceed bounds.  Reorganize attributes and extensions to separate files.
    
    * Extract UserDefined extension
    
    * More work on extensions
    
    * Add extension return data
    
    * Collapse extension seeking with multi pattern match
    
    * More extension matching
    
    * Added several extension enum values.  Simplified extension matching.  Preliminary DataTypeType retrieval.
    
    * More extension type mapping
    
    * Add publish action
    
    * Publish
    
    * Add Output extension type
    
    * Fold in attributes section with Extensions.  Work on cleanin up return data.
    
    * Explode aaobject: output header
    
    * Minor cleanup to allow serializing AaObject back to file
    
    * Added some post-extension items back in for templates.  Convert a few more functions to use _seek_bytes.
    
    * Rough-in printout of script body text to separate file.

---
+++ tests/sputility_test.py
@@ -0,0 +1,55 @@
+import glob
+import os
+import pprint
+import unittest
+
+from sputility import *
+
+# Shared paths
+LOCAL_BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+LOCAL_INPUT_PATH = os.path.join(LOCAL_BASE_PATH, 'input_files')
+LOCAL_OUTPUT_PATH = os.path.join(LOCAL_BASE_PATH, 'output_files')
+
+LOCAL_INPUT_AAOBJECT_PATH = os.path.join(LOCAL_INPUT_PATH, 'aaobject')
+LOCAL_INPUT_AAPKG_PATH = os.path.join(LOCAL_INPUT_PATH, 'aapkg')
+LOCAL_OUTPUT_AAOBJECT_PATH = os.path.join(LOCAL_OUTPUT_PATH, 'aaobject')
+LOCAL_OUTPUT_AAPKG_PATH = os.path.join(LOCAL_OUTPUT_PATH, 'aapkg')
+
+class sputility_tests(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_aapkg_to_folder(self):
+        print('')
+        for file in glob.glob(os.path.join(LOCAL_INPUT_AAPKG_PATH, '*.aaPKG')):
+            spu = SPUtility()
+            resp = spu.decompress_package(
+                input_path=file,
+                output_path=LOCAL_OUTPUT_AAPKG_PATH,
+                progress=None
+            )
+            pprint.pprint(resp)
+
+    def test_aaobject_to_folder(self):
+        print('')
+        for file in glob.glob(os.path.join(LOCAL_INPUT_AAOBJECT_PATH, '*.txt')):
+            spu = SPUtility()
+            print(file)
+            resp = spu.explode_object(
+                input_path=file,
+                output_path=LOCAL_OUTPUT_AAOBJECT_PATH,
+                progress=None
+            )
+            print(f'Parsed {resp.offset:0X} of {resp.size:0X} bytes')
+            #pprint.pprint(resp.header)
+            for section in resp.extensions:
+                #print(f'Primitive name: {section.primitive_name}, Count: {len(section.attributes)}')
+                #for attr in section.attributes:
+                #    print(f'Attribute ID: {attr.id}')
+                #for attr in section.attributes:
+                #    print(attr.id)
+                #pprint.pprint(section)
+                pass
+
+    def tearDown(self):
+        pass
